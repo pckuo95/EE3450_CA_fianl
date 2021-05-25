e7129008036e2670277670f6aaacfa8071790080
pckuo95 <pckuo95@gmail.com>
2021-05-25 14:40:14 +0800

    finish gcdA.c

--- gcdA.c
+++ gcdA.c
@@ -9,11 +9,20 @@
 #include <stdio.h>
 
 int gcd(int a, int b) {
-    /*
-     *
-     *  Finish your code here
-     * 
-     */
+
+    // check terminated (E2 path)
+    if (a == b) {
+        return a;
+    }
+    // check recursive (E1 path, A > B)
+    else if (a > b) {
+        return gcd(a - b, b);
+    }
+    // check recursive (E1 path, B > A)
+    else {
+        return gcd(a, b - a);
+    }
+
 }
 
 int main(void) {
