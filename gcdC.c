226c217479e3decad026b28d8c7115e243aa2bff
pckuo95 <pckuo95@gmail.com>
2021-05-25 15:14:13 +0800

    finish gcdC.c

--- gcdC.c
+++ gcdC.c
@@ -7,11 +7,27 @@
 #include <stdio.h>
 
 int gcd(int a, int b) {
-    /*
-     *
-     *  Finish your code here
-     * 
-     */
+    int n = 0;
+    while ((a % 2) == 0 && (b % 2) == 0) {
+        a = a >> 1;
+        b = b >> 1;
+        n++;
+    }
+
+    while (a != b) {
+        // 'a', 'b' not equal(E2 condition), check which is bigger
+        if (a > b) {
+            // 'a' bigger, iterative for 'a' 
+            a = a - b;
+        }
+        else {
+            // 'b' bigger, iterative for 'b', no need to consider equal
+            b = b - a;
+        }
+    }
+
+    a = a << n;
+    return a;
 }
 
 int main(void) {
